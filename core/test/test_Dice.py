681842c5319d45088956f3da9b146755c4c7bfdf
Diego Aubone <diegoaubone8@gmail.com>
2025-08-26 20:36:11 -0300

    Implementacion de metodos de clase dice y test

---
+++ core/test/test_Dice.py
@@ -0,0 +1,12 @@
+
+import unittest
+from core.clases.Dice import Dice  
+
+class TestDice(unittest.TestCase):
+
+    def test_valores_en_rango(self):
+        dice = Dice()
+        for _ in range(100):  
+            dado1, dado2 = dice.lanzar_dados()
+            self.assertIn(dado1, range(1, 7))
+            self.assertIn(dado2, range(1, 7))
